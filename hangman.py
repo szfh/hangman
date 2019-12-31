f239447868a7a4582ab7d8d91479da658b3dd984
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-31 18:55:07 +0000

    set up import_word_list function

--- hangman.py
+++ hangman.py
@@ -5,3 +5,11 @@ Created on Tue Dec 31 18:18:28 2019
 @author: Sam
 """
 
+def import_word_list():
+    """import word list from .txt file"""
+    print("Importing word list")
+    # code here
+    print("Import complete")
+    
+# main program starts here
+    import_word_list()
