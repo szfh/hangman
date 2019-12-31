cc9f5f2d1fb4e75eb332d72d6f27dd1dd74b8d31
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-31 19:54:32 +0000

    select word

--- hangman.py
+++ hangman.py
@@ -4,6 +4,7 @@ Created on Tue Dec 31 18:18:28 2019
 
 @author: Sam
 """
+from random import randint
 
 def import_word_list():
     """import word list from .txt file"""
@@ -16,10 +17,15 @@ def import_word_list():
     return word_list
 
 def select_word(word_list):
-    # code here
-    return word_list[1]
+    """select a random word from the list"""
+    n = randint(0,99)
+    return word_list[n].strip()
 
 # main program starts here
     
 word_list = import_word_list()
 selected_word = select_word(word_list)
+word_length = len(selected_word)
+print(selected_word)
+
+#python strings https://www.w3schools.com/python/python_strings.asp
