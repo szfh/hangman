6a7b660bf0ad03261c948da05e77457630dab98e
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-01-01 09:30:59 +0000

    better variable names

--- hangman.py
+++ hangman.py
@@ -24,20 +24,26 @@ def select_word(word_list):
 # main program starts here
     
 word_list = import_word_list()
-selected_word = select_word(word_list)
-word_length = len(selected_word)
+word = select_word(word_list)
+word_length = len(word)
 
 # python strings https://www.w3schools.com/python/python_strings.asp
 
 # next: display letters as asterisks
 # then: prompt for first guess
 
-unsolved_word = "*" * word_length
+word_unsolved = "*" * word_length
 all_guesses = []
 
-n = 0
-while n < 3:
-    print("the current word is: ", unsolved_word)
+g = 0
+while g < 3:
+    print("the current word is: ", word_unsolved)
     guess = input("Please enter your next guess: ")
     all_guesses.append(guess)
-    n += 1
+    
+    #for l in word_length
+    #if selected_word[l] in guess
+    #unsolved_word[l] = guess
+    
+    
+    g += 1 # only if incorrect guess
