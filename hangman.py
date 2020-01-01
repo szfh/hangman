296b7d9d4b8fb0c46c14f94944293c6982ddae60
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-01-01 13:06:01 +0000

    update word unsolved

--- hangman.py
+++ hangman.py
@@ -5,6 +5,7 @@ Created on Tue Dec 31 18:18:28 2019
 @author: Sam
 """
 from random import randint
+from string import ascii_lowercase
 
 def import_word_list():
     """import word list from .txt file"""
@@ -34,34 +35,55 @@ word_length = len(word)
 
 word_unsolved = "*" * word_length
 all_guesses = []
-
 lives = 3
+
 while lives > 0:
     print("You have %d lives remaining. The current word is: %s" % (lives, word_unsolved))
     guess = input("Please enter your next guess: ")
-    # error check
+    # error check - exactly 1 character, letter a-z
     all_guesses.append(guess)
     
     if guess in word:
         print("Correct")
-        # update word_unsolved
         # breakpoint()
-        l=0
-        # for l in word_length:
+        # l=0
+        # for l in range(word_length):
             # breakpoint()
+            # print(l)
+            # print(word[l])
             # if word[l] in guess:
-                # breakpoint()
-                # word_unsolved[l] = guess
+#                 # breakpoint()
+#                 word_unsolved[l] = guess
         
     else:
         print("Incorrect")
-        # take away a life
+#         # take away a life
+    
+    # update word_unsolved
+    word_unsolved = str()
+    for i in range(word_length):
+        if word[i] in all_guesses:
+            word_unsolved = word_unsolved + word[i]
+        else:
+            word_unsolved = word_unsolved + "*"
     
     
-    
-    #for l in word_length
-    #if selected_word[l] in guess
-    #unsolved_word[l] = guess
+    # #for l in word_length
+    # #if selected_word[l] in guess
+    # #unsolved_word[l] = guess
     
     
     lives -= 1 # only if incorrect guess
+    
+# test
+# test_lives = 3
+# test_all_guesses = ["a","b","c"]
+# # test_not_guessed = ascii_lowercase - test_all_guesses
+# test_word = "sugar"
+# test_word_unsolved = "*" * 5
+
+# for l in range(5):
+#     print("letter = %s" %test_word[l])
+#     if test_word[l] == "a":
+#         print("l = %d" %l)
+    
