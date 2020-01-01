ca09aca007be279f1d4a3059cfcd9d1b0ee05688
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-01-01 14:17:27 +0000

    random game and 7 lives

--- hangman.py
+++ hangman.py
@@ -5,7 +5,6 @@ Created on Tue Dec 31 18:18:28 2019
 @author: Sam
 """
 from random import randint
-# from string import ascii_lowercase
 
 def select_word():
     """import word_list.txt, then return a random word"""
@@ -14,7 +13,7 @@ def select_word():
     f.close()
     
     n = randint(0,99)
-    word = word_list[3].strip()
+    word = word_list[n].strip()
     return word
 
 def game(word, all_guesses):
@@ -29,9 +28,8 @@ def game(word, all_guesses):
 # main program starts here
 word = select_word()
 word_length = len(word)
-# word_unsolved = "*" * word_length
 all_guesses = []
-lives = 3
+lives = 7
 
 while True:
     word_unsolved = game(word, all_guesses)
